497d3eb8d5bd5399cf0e9072f4e10dbe23155047
kiki30 <3283763959@qq.com>
2025-10-24 09:59:11 +0800

    lab9 fs bigfile

--- kernel/fs.c
+++ kernel/fs.c
@@ -400,6 +400,35 @@ bmap(struct inode *ip, uint bn)
     brelse(bp);
     return addr;
   }
+  bn -= NINDIRECT;
+
+  // 二级间接块
+  if(bn < NINDIRECT * NINDIRECT){
+    // 读取二级间接块
+    if((addr = ip->addrs[NDIRECT + 1]) == 0)
+      ip->addrs[NDIRECT + 1] = addr = balloc(ip->dev);
+    bp = bread(ip->dev, addr);
+    a = (uint*)bp->data;
+    
+    // 计算在二级间接块中的索引（第几个一级间接块）
+    if((addr = a[bn / NINDIRECT]) == 0){
+      a[bn / NINDIRECT] = addr = balloc(ip->dev);
+      log_write(bp);
+    }
+    brelse(bp);
+    
+    // 读取一级间接块
+    bp = bread(ip->dev, addr);
+    a = (uint*)bp->data;
+    
+    // 计算在一级间接块中的索引（数据块）
+    if((addr = a[bn % NINDIRECT]) == 0){
+      a[bn % NINDIRECT] = addr = balloc(ip->dev);
+      log_write(bp);
+    }
+    brelse(bp);
+    return addr;
+  }
 
   panic("bmap: out of range");
 }
@@ -430,6 +459,40 @@ itrunc(struct inode *ip)
     brelse(bp);
     bfree(ip->dev, ip->addrs[NDIRECT]);
     ip->addrs[NDIRECT] = 0;
+  }
+
+  // 释放二级间接块
+  if(ip->addrs[NDIRECT + 1]){
+    struct buf *bp2;
+    uint *a2;
+    
+    // 读取二级间接块
+    bp = bread(ip->dev, ip->addrs[NDIRECT + 1]);
+    a = (uint*)bp->data;
+    
+    // 遍历所有一级间接块
+    for(i = 0; i < NINDIRECT; i++){
+      if(a[i]){
+        // 读取一级间接块
+        bp2 = bread(ip->dev, a[i]);
+        a2 = (uint*)bp2->data;
+        
+        // 释放所有数据块
+        for(j = 0; j < NINDIRECT; j++){
+          if(a2[j])
+            bfree(ip->dev, a2[j]);
+        }
+        brelse(bp2);
+        
+        // 释放这个一级间接块
+        bfree(ip->dev, a[i]);
+      }
+    }
+    brelse(bp);
+    
+    // 释放二级间接块本身
+    bfree(ip->dev, ip->addrs[NDIRECT + 1]);
+    ip->addrs[NDIRECT + 1] = 0;
   }
 
   ip->size = 0;
